dba4d77635329d4ebab83e9099bba6429d8f8c25
Abdullah Usta <61752854+AbdullahUsta@users.noreply.github.com>
2024-04-16 02:08:05 +0400

    Update index.js
    Added multiple requests answers.

--- express_server/index.js
+++ express_server/index.js
@@ -3,9 +3,18 @@ const port = 3000;
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("test");
+    res.send("<h1>Main Page</h1>");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("<h1>+971585965507, +905300745501</h1>");
+});
+
+app.get("/about", (req, res) => {
+    res.send("<p>I am Abdullah USTA, a web developer!</p>");
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
